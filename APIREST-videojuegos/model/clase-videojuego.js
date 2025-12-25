1eb2d89d269389190b83a4efd60bed9cc75cfd9d
lluisfsegui <lluisfelipsegui@paucasesnovescifp.cat>
2025-12-25 20:58:14 +0100

    Se me olvidó crear la carpeta model donde voy a crear las clases para poder tener la estructura para poder usar las variables

---
+++ APIREST-videojuegos/model/clase-videojuego.js
@@ -0,0 +1,11 @@
+class videojoc {
+    constructor(id, titulo, genero, platforma, year) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.platforma = platforma;
+        this.year = year;
+    }
+}
+
+module.exports = videojoc;
